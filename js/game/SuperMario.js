c4a01272576efc1921b89c8f5592374848a4e872
projetprog2013 <projetprog2013@gmail.com>
2013-03-21 00:53:15 +0100

    Ajout bloc + indentation avec tabulations

--- js/game/SuperMario.js
+++ js/game/SuperMario.js
@@ -1,40 +1,43 @@
 var SuperMario = {
-  start: function() {
-    Game.infoGameBuilder = false;
-    Game.setRooms([new Room(592,400)]);
+	start: function() {
+		Game.infoGameBuilder = false;
+		Game.setRooms([new Room(592,400)]);
+		Game.gameStart = function()
+		{
+			// Compteur de pièces à 0
+			Piece.counter = 0;
 
-    Game.gameEnd = function() {
-    };
-    Game.gameStart = function()
-    {
-      // Compteur de pièces à 0
-      Piece.counter = 0;
+			// Suppression des instances créées dans les parties précédentes
+			var types = [Piece, PieceController, Mario, Bloc];
+			_.each(types, function(t) {
+				var instances = Game.getInstancesByType(t);
 
-      // Suppression des instances créées dans les parties précédentes
-      var types = [Piece, PieceController, Mario, Bloc];
-      _.each(types, function(t) {
-        var instances = Game.getInstancesByType(t);
+				_.each(instances, function(p) {
+					Game.instanceDestroy(p);
+				});
+			});
 
-        _.each(instances, function(p) {
-          Game.instanceDestroy(p);
-        });
-      });
-
-      // Création des différents éléments
-      Game.instanceCreate(280,192,Mario);
-      Game.instanceCreate(10,13,PieceController);
-      Game.instanceCreate(200,192,Piece);
-      Game.instanceCreate(250,192,Piece);
-      Game.instanceCreate(300,192,Piece);
-      Game.instanceCreate(350,192,Piece);
-      Game.instanceCreate(250,130,BlocSpecial);
-    };
-    Game.loadAndRun('jeu',
-    {
-      'marioSpriteLeft': 'img/game/marioSpriteLeft.png',
-      'marioSpriteRight': 'img/game/marioSpriteRight.png',
-      'blocSpecialSprite': 'img/game/blocSpecialSprite.png',
-      'coinSprite': 'img/game/coinSprite.png'
-    });
-  }
-}
+			// Création des différents éléments
+			Game.instanceCreate(280,192,Mario);
+			Game.instanceCreate(10,13,PieceController);
+			Game.instanceCreate(200,192,Piece);
+			Game.instanceCreate(250,192,Piece);
+			Game.instanceCreate(300,192,Piece);
+			Game.instanceCreate(350,192,Piece);
+			Game.instanceCreate(250,130,BlocSpecial);
+			
+			// En guise de sol...
+			Game.instanceCreate(280,240,BlocSpecial);
+		};
+		Game.gameEnd = function() {
+		};
+		Game.loadAndRun('jeu',
+			{
+				'marioSpriteLeft': 'img/game/marioSpriteLeft.png',
+				'marioSpriteRight': 'img/game/marioSpriteRight.png',
+				'blocSpecialSprite': 'img/game/blocSpecialSprite.png',
+				'coinSprite': 'img/game/coinSprite.png'
+			}
+		);
+	}
+};
