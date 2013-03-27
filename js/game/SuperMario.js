f5bcf0ff2b65c7a1f7decd59d5b782a4f66185fb
projetprog2013 <projetprog2013@gmail.com>
2013-03-27 01:41:36 +0100

    Update Koopa + Bloc names

--- js/game/SuperMario.js
+++ js/game/SuperMario.js
@@ -1,22 +1,44 @@
 var SuperMario = {
-	types: [
-		Piece,
-		PieceController,
-		Mario,
-		Bloc,
-		Terre,
-		SousTerre,
-		BlocSpecial
-	],
 	start: function() {
 		Game.infoGameBuilder = false;
 		
 		Piece.counter = 0;
-		var level = new Room('js/game/level.xml');
+		var level = new Room('level.xml');
 		level.view_w = 592;
 		Game.setRooms([level]);
 		Game.lilo = false;	
 		this.gameOverSound = new buzz.sound("sound/game/gameover.wav");
+		
+		/*
+		Game.setRooms([new Room(592,400)]);
+		Game.gameStart = function()
+		{
+			// Compteur de pièces à 0
+			Piece.counter = 0;
+
+			// Suppression des instances créées dans les parties précédentes
+			var types = [Piece, PieceController, Mario, Bloc];
+			_.each(types, function(t) {
+				var instances = Game.getInstancesByType(t);
+
+				_.each(instances, function(p) {
+					Game.instanceDestroy(p);
+				});
+			});
+
+			// Création des différents éléments
+			Game.instanceCreate(280,192,Mario);
+			Game.instanceCreate(10,13,PieceController);
+			Game.instanceCreate(200,192,Piece);
+			Game.instanceCreate(250,192,Piece);
+			Game.instanceCreate(300,192,Piece);
+			Game.instanceCreate(350,192,Piece);
+			Game.instanceCreate(250,130,BlocSpecial);
+			
+			// En guise de sol...
+			Game.instanceCreate(280,240,BlocSpecial);
+		};
+		*/
 		
 		Game.gameEnd = function() {
 		};
@@ -39,20 +61,6 @@ var SuperMario = {
 
 		// sauvegarde de partie etc?
 		Game.end = true;
-
-		// Compteur de pièces à 0
-		Piece.counter = 0;
-
-
-		var types = this.types;
-		// Suppression des instances créées dans les parties précédentes
-		_.each(types, function(t) {
-			var instances = Game.getInstancesByType(t);
-
-			_.each(instances, function(p) {
-				Game.instanceDestroy(p);
-			});
-		});
 		
 		// redirection vers la page de game over
 		document.location.href = "#/game-over";
