682f8b81cf22f969184c5f792e4f89a9134f6a2d
projetprog2013 <projetprog2013@gmail.com>
2013-03-29 07:51:55 +0100

    Ajout Athlectic Theme

--- js/game/SuperMario.js
+++ js/game/SuperMario.js
@@ -1,6 +1,11 @@
 var SuperMario = {
-	types: ['Air', 'Terre1', 'Terre2', 'Terre3', 'Terre4', 'Terre5', 'Terre6', 'Terre7', 'Terre8', 'Terre9', 'Terre10', 'Terre11', 'Terre12', 'Terre13', 'Terre14', 'Terre15', 'Terre16', 'Terre17', 'Terre18', 'Terre19', 'Terre20', 'Terre21', 'Terre22', 'Terre23', 'Terre24', 'Piece', 'TuyauV0', 'TuyauV1', 'TuyauV2', 'TuyauV3', 'TuyauV4', 'TuyauV5', 'TuyauH0', 'TuyauH1', 'TuyauH2', 'TuyauH3', 'TuyauH4', 'TuyauH5', 'BlocSpecial', 'BlocTourne', 'Mario', 'Koopa'],
+	types: ['Air', 'Terre1', 'Terre2', 'Terre3', 'Terre4', 'Terre5', 'Terre6', 'Terre7', 'Terre8', 'Terre9', 'Terre10', 'Terre11', 'Terre12', 'Terre13', 'Terre14', 'Terre15', 'Terre16', 'Terre17', 'Terre18', 'Terre19', 'Terre20', 'Terre21', 'Terre22', 'Terre23', 'Terre24', 'Piece', 'TuyauV0', 'TuyauV1', 'TuyauV2', 'TuyauV3', 'TuyauV4', 'TuyauV5', 'TuyauH0', 'TuyauH1', 'TuyauH2', 'TuyauH3', 'TuyauH4', 'TuyauH5', 'BlocSpecial', 'BlocTourne', 'Mario', 'Koopa', 'Goomba'],
 	// retourne l'object contenant les parties sauvegardées
+	
+	gameOverSound: new buzz.sound("sound/game/gameover.wav"),
+	
+	athleticTheme: new buzz.sound("sound/themes/Athletic-Theme.wav"), // Si c'est trop lourd, on peut utiliser la version mp3
+
 	savedGames: function() {
 		var games = localStorage['saved_games'];
 
@@ -67,7 +72,7 @@ var SuperMario = {
 		level.view_w = 592;
 		Game.setRooms([level]);
 		Game.lilo = false;	
-		this.gameOverSound = new buzz.sound("sound/game/gameover.wav");
+		this.athleticTheme.play();
 		
 		Game.gameEnd = function() {
 		};
@@ -100,6 +105,8 @@ var SuperMario = {
 		}
 	},
 	gameOver: function() {
+		this.athleticTheme.stop();
+		
 		// Musique de fin
 		this.gameOverSound.play();
 		
